0e264bd66b7c99325af1c555007c1bd70ef822f2
Maldonado <maldonadoq.percy@gmail.com>
2019-09-27 00:18:56 -0500

    recommender

---
+++ recommender/src/main/java/maldonado/recommender/TRecommender.java
@@ -0,0 +1,43 @@
+package maldonado.recommender;
+
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class TRecommender {
+    public static void main(String[] args) throws IOException, TasteException {
+        // Load historical data about user preferences
+        DataModel model = new FileDataModel(new File("/home/maldonado/Cloud/data/book.csv"));
+
+        // Compute the similarity between users, according to their preferences
+        UserSimilarity similarity = new EuclideanDistanceSimilarity(model);
+
+        // Group the users with similar preferences
+        UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1,
+                similarity, model);
+
+        // Create a recommender
+        UserBasedRecommender recommender = new GenericUserBasedRecommender(
+                model, neighborhood, similarity);
+
+        // For the user with the id 1 get two recommendations
+        List<RecommendedItem> recommendations = recommender.recommend(1, 4);
+        for (RecommendedItem recommendation : recommendations) {
+            System.out.println("User 1 might like the book with ID: "
+                    + recommendation.getItemID() + " (predicted preference :"
+                    + recommendation.getValue() + ")");
+        }
+    }
+}
